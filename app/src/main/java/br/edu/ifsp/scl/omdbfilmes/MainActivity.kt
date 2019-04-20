428681d49a3ea5a466d18c0be41bb0056ac346bd
luiz1988 <luiz-fernando1988@hotmail.com>
2019-04-20 16:30:05 -0300

    Implementação do layout fragment e o metódo responsável para exibi-lo na mainActivity.

--- app/src/main/java/br/edu/ifsp/scl/omdbfilmes/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/omdbfilmes/MainActivity.kt
@@ -2,13 +2,15 @@ package br.edu.ifsp.scl.omdbfilmes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
+import br.edu.ifsp.scl.omdbfilmes.fragments.FragmentInicial
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar_layout.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,32 +29,37 @@ class MainActivity : AppCompatActivity() {
                 R.string.menu_fechado
             )
         menuLateralDrawerLayout.addDrawerListener(abreFechaToogle)
+
         abreFechaToogle.syncState()
 
         menuNavigationView.setNavigationItemSelectedListener { onNavigationItemSelected(it) }
 
+        SetarFragment(FragmentInicial())
 
     }
 
     private fun onNavigationItemSelected(item: MenuItem): Boolean {
         var retorno: Boolean = false
         when(item.itemId){
-//            R.id.modoTextoMenuItem -> {
-//                substituiFragment(MODO_TEXTO)
-//                retorno = true
-//            }
-//            R.id.modoGraficoMenuItem -> {
-//                substituiFragment(MODO_GRAFICO)
-//                retorno = true
-//            }
             R.id.sairMenuItem -> {
                 finish()
                 retorno = true
             }
         }
         menuLateralDrawerLayout.closeDrawer(GravityCompat.START)
-
         return retorno
-
     }
+
+    private fun SetarFragment(fragment: Fragment){
+
+
+        val fragmentTarget = fragment;
+
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,  fragmentTarget)
+        fragmentTransaction.commit()
+
+}
+
 }
---
+++ app/src/main/java/br/edu/ifsp/scl/omdbfilmes/fragments/FragmentInicial.kt
@@ -0,0 +1,19 @@
+package br.edu.ifsp.scl.omdbfilmes.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.edu.ifsp.scl.omdbfilmes.R
+
+class FragmentInicial: Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val layoutView = inflater.inflate(R.layout.fragment_inicial, null)
+
+        return layoutView
+    }
+
+}
